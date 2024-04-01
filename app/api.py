c2085171bdbb9510fca2123bba8e6a112d86c8ef
thiagoabreu <thiago.abreu@zgsolucoes.com.br>
2024-03-31 21:00:34 -0300

    feat: implementação para permitir escalar o serviços utilizando redis

--- app/api.py
+++ app/api.py
@@ -2,6 +2,17 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+import redis, json
+
+import os, sys
+senha_redis = os.environ.get('REDIS_PASSWORD')
+if not senha_redis:
+    print("Erro: a senha do REDIS não foi informada!")
+
+
+redisRW = redis.Redis(host='redis-master.servicos.svc.cluster.local', port=6379, db=0, password=senha_redis, decode_responses=True)
+redisRO = redis.Redis(host='redis-replicas.servicos.svc.cluster.local', port=6379, db=0, password=senha_redis, decode_responses=True)
+# 4GgQsPDSVU
 
 app_name = 'comentarios'
 app = Flask(app_name)
@@ -23,10 +34,12 @@ def api_comment_new():
             'comment': comment,
             }
 
-    if content_id in comments:
-        comments[content_id].append(new_comment)
-    else:
-        comments[content_id] = [new_comment]
+    ## Para escrita utilizar o redis master
+    redisRW.rpush(content_id, json.dumps(new_comment))
+    # if content_id in comments:
+    #     comments[content_id].append(new_comment)
+    # else:
+    #     comments[content_id] = [new_comment]
 
     message = 'comment created and associated with content_id {}'.format(content_id)
     response = {
@@ -40,14 +53,28 @@ def api_comment_new():
 def api_comment_list(content_id):
     content_id = '{}'.format(content_id)
 
-    if content_id in comments:
-        return jsonify(comments[content_id])
+    # if content_id in comments:
+    #     return jsonify(comments[content_id])
+    # else:
+    #     message = 'content_id {} not found'.format(content_id)
+    #     response = {
+    #             'status': 'NOT-FOUND',
+    #             'message': message,
+    #             }
+    #     return jsonify(response), 404
+    # Recupera todos os comentários associados ao content_id
+
+    comments = redisRO.lrange(content_id, 0, -1)
+    comments = [json.loads(comment) for comment in comments] if comments else []
+
+    if comments:
+        return jsonify(comments)
     else:
         message = 'content_id {} not found'.format(content_id)
         response = {
-                'status': 'NOT-FOUND',
-                'message': message,
-                }
+            'status': 'NOT-FOUND',
+            'message': message,
+        }
         return jsonify(response), 404
 
 
